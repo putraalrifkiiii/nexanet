4bb1af68b12089bf9b82ac7571e0db7a1b03fd42
putraalrifkiiii <putraalrifkiiii19@gmail.com>
2026-09-20 23:44:53 +0700

    feat: implement PaketCard component in HomePage for displaying internet packages

--- frontend/src/components/ui/PaketCard.tsx
+++ frontend/src/components/ui/PaketCard.tsx
@@ -9,9 +9,12 @@ const idr = (value: number) =>
 
 interface PaketCard1Props {
   pkg: PaketUI;
+  variant?: "default" | "compact";
 }
 
-const PaketCard = ({ pkg }: PaketCard1Props) => {
+const PaketCard = ({ pkg, variant = "default" }: PaketCard1Props) => {
+  const isCompact = variant === "compact";
+
   return (
     <div
       className={`p-6 flex flex-col rounded-2xl ${pkg.top ? "bg-brand-dark border-none" : "bg-brand-white border border-brand-border"}`}
@@ -46,51 +49,57 @@ const PaketCard = ({ pkg }: PaketCard1Props) => {
       >
         per bulan
       </div>
-      <div
-        className={`text-xs mb-4 font-body bg-green-600 text-brand-white py-1 px-2 self-start rounded-xl`}
-      >
-        Biaya pasang:{" "}
-        {pkg.biaya_pemasangan === 0 ? "Gratis" : idr(pkg.biaya_pemasangan)}
-      </div>
+      {!isCompact && (
+        <div
+          className={`text-xs mb-4 font-body bg-green-600 text-brand-white py-1 px-2 self-start rounded-xl`}
+        >
+          Biaya pasang:{" "}
+          {pkg.biaya_pemasangan === 0 ? "Gratis" : idr(pkg.biaya_pemasangan)}
+        </div>
+      )}
+
       <p
         className={`text-xs leading-relaxed mb-6 flex-1 font-body ${pkg.top ? "text-[rgba(255,255,255,0.35)]" : "text-brand-muted"}`}
       >
         {pkg.deskripsi_paket}
       </p>
-      <div
-        className={`pt-5 mb-6 space-y-2.5  ${pkg.top ? "border-t border-brand-white" : "border-t border-brand-border"}`}
-      >
-        {pkg.fitur.map((f) => (
-          <div key={f} className="flex items-start gap-2">
-            <svg
-              width="14"
-              height="14"
-              viewBox="0 0 14 14"
-              className="shrink-0 mt-0.5"
-              fill="none"
-            >
-              <circle
-                cx="7"
-                cy="7"
-                r="7"
-                fill={pkg.top ? "rgba(20,86,240,0.25)" : "#F3F4F6"}
-              />
-              <path
-                d="M4.5 7l2 2 3-3"
-                stroke={pkg.top ? "#60a5fa" : "#6B7280"}
-                strokeWidth="1.2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <span
-              className={`text-xs leading-relaxed font-body ${pkg.top ? "text-[rgba(255,255,255,0.5)]" : "text-brand-muted"}`}
-            >
-              {f}
-            </span>
-          </div>
-        ))}
-      </div>
+
+      {!isCompact && (
+        <div
+          className={`pt-5 mb-6 space-y-2.5  ${pkg.top ? "border-t border-brand-white" : "border-t border-brand-border"}`}
+        >
+          {pkg.fitur.map((f) => (
+            <div key={f} className="flex items-start gap-2">
+              <svg
+                width="14"
+                height="14"
+                viewBox="0 0 14 14"
+                className="shrink-0 mt-0.5"
+                fill="none"
+              >
+                <circle
+                  cx="7"
+                  cy="7"
+                  r="7"
+                  fill={pkg.top ? "rgba(20,86,240,0.25)" : "#F3F4F6"}
+                />
+                <path
+                  d="M4.5 7l2 2 3-3"
+                  stroke={pkg.top ? "#60a5fa" : "#6B7280"}
+                  strokeWidth="1.2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                />
+              </svg>
+              <span
+                className={`text-xs leading-relaxed font-body ${pkg.top ? "text-[rgba(255,255,255,0.5)]" : "text-brand-muted"}`}
+              >
+                {f}
+              </span>
+            </div>
+          ))}
+        </div>
+      )}
       <button
         type="button"
         className={`w-full py-3 text-sm font-semibold cursor-pointer hover:opacity-85 transition-opacity font-display text-brand-white rounded-[10px] ${pkg.top ? "bg-brand-blue" : "bg-brand-dark"}`}
--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -2,13 +2,67 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import FeatureSection from "@/components/sections/FeatureSection";
+import { PaketCard } from "@/components/paket-wifi";
+import { useState, useEffect } from "react";
+import { mapPaketToUI, type PaketUI } from "@/constants/paketMapper";
+import { fetchPaketWifi } from "@/api/paketApi";
 
 const HomePage = () => {
+  const [pkgs, setPkgs] = useState<PaketUI[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    async function loadPaketWifi() {
+      try {
+        const dataApi = await fetchPaketWifi();
+        setPkgs(mapPaketToUI(dataApi));
+      } catch {
+        setError("Paket internet belum dapat dimuat bro.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    void loadPaketWifi();
+  }, []);
+
   return (
     <div>
       <Navbar />
       <HeroSection />
       <FeatureSection />
+      <div className="bg-brand-offwhite h-fit ">
+        <div className="max-w-7xl mx-auto px-5 sm:px-8 py-16 sm:py-24">
+          <div className="uppercase tracking-widest mb-4 font-mono text-brand-muted text-[11px]">
+            Paket Internet
+          </div>
+          <div className="flex flex-col sm:flex-row  justify-between mb-10 items-center">
+            <h1 className="text-3xl sm:text-4xl font-extrabold mb-4 leading-tight font-display text-brand-dark">
+              Pilih Kecepatanmu.
+            </h1>
+            <h1 className="text-sm font-body text-brand-blue hover:underline cursor-pointer mt-2">
+              Bandingkan semua paket →
+            </h1>
+          </div>
+
+          {isLoading && (
+            <p className="text-sm font-body text-brand-muted">
+              Memuat paket internet...
+            </p>
+          )}
+          {error && !isLoading && (
+            <p className="text-sm font-body text-brand-danger">{error}</p>
+          )}
+          {!isLoading && !error && (
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 ">
+              {pkgs.map((pkg) => (
+                <PaketCard key={pkg.id} pkg={pkg} variant="compact" />
+              ))}
+            </div>
+          )}
+        </div>
+      </div>
       <Footer />
     </div>
   );
